597ec75d85d0c798c6aea4d8faf86e1220372c97
Salhe Seid <109861854+salhe123@users.noreply.github.com>
2023-05-08 12:49:51 +0300

    Add files via upload

---
+++ Portfolio/portfolio1/README.txt
@@ -0,0 +1,6 @@
+Hey! My Name is Salhe seid and now i will show you how can 
+operate this portfolio website.
+
+the website shows the some personal information about the writer 
+and can be open by two choises.The one is simply open any browser and the 2nd 
+open by the server like Wamp and Xampp server simply.
---
+++ Portfolio/portfolio2/home.php
@@ -0,0 +1,139 @@
+<html>
+    <head>
+        <title>Portfolio Website</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="hero">
+            <nav>
+                <h2 class="logo">Portfo<span>lio</span></h2>
+                <ul>
+                    <li><a href="#">home</a> </li>
+                    <li><a href="#">About Us</a> </li>
+                    <li><a href="#">Services</a> </li>
+                    <li><a href="#">Skills</a> </li>
+                    <li><a href="#">Contact Us</> </li>
+                </ul>
+                <a href="#" class="btn">Subscribe</a> 
+            </nav>
+            <div class="content">
+                <h4>Hello, my name is</h4>
+                <h1>Salhe <span>Seid</span></h1>
+                <h3>I'am a Web Developer</h3>
+                <div class="newslatter">
+                    <form>
+                      <input type="email" name="email" id="mail" placeholder="Enter Your Email">
+                      <input class="try" type="submit" name="submit" value="lets start">
+                    
+                    </form>
+
+                </div>
+            </div>
+        </div>
+           <section class="about">
+               <div class="main">
+                   <img src="section.jpg">
+                   <div class="about-text">
+                       <h2>About Me</h2>
+                       <h5>Developer <span>& Designer</span> </h5>
+                       <p>I am a full web developer.I can provide clean code and pixel 
+                           perect design.I also make the website more & more interactive with web
+                           animations.I can also provide clean code and pixel perfect design,I also make
+                           the website more & more interactive with web animations.A responsive design
+                           makes your website accessible to all users, regardeless of their device.
+                           and also make it connect its server side.
+                       </p>
+                       <button type="button">Let's Talk</button>
+
+                   </div>
+
+               </div>
+
+           </section>
+           <div class="service">
+               <div class="title">
+                   <h2>Our Services</h2>
+
+               </div>
+               <div class="box">
+                   <div class="card">
+                       <i class="fas fa-bars"></i>
+                       <h5>Web Development</h5>
+                       <div class="pra">
+                           <p>Every Website should be built with two primarygoals:Firstly, it needs
+                               to work across all devices. secondly, it needs to be fast as possible.
+                           </p>
+
+
+                           <p style="text-align: center;">
+                               <a class="button" href="#">Read more</a>
+                           </p>
+
+
+                       </div>
+
+                   </div>
+
+
+
+                   <div class="card">
+                    <i class="fas fa-bars"></i>
+                    <h5>Web Development</h5>
+                    <div class="pra">
+                        <p>When you built a website there is so many things that fulfill 
+                            in the body of the website.Which used to access information 
+                            easly.
+                        </p>
+
+
+                        <p style="text-align: center;">
+                            <a class="button" href="#">Read more</a>
+                        </p>
+
+
+                    </div>
+
+                </div>
+                <div class="card">
+                    <i class="fas fa-bars"></i>
+                    <h5>Web Development</h5>
+                    <div class="pra">
+                        <p>If aperson wants to learn any programming language that used to
+                            access knowlodege websites is the best center .So in order 
+                            to build awebsite must be learn HTML,CSS,JS and PHP.
+                        </p>
+
+
+                        <p style="text-align: center;">
+                            <a class="button" href="#">Read more</a>
+                        </p>
+
+
+                    </div>
+
+                </div>
+                
+               </div>
+               
+           </div>
+           <div class="contact">
+               <p>Let Me Get You A Beautifull Website</p>
+               <a class="button-two" href="#">Hire Me</a>
+
+           </div>
+           <!--footer-->
+          <footer>
+              <p>Salhe Seid</p>
+              <p>For more Information about me- please on the link below to
+                  hire me.
+              </p>
+              <div class="social">
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+
+            </div>
+            <p class="end">Copy right by Salhe Seid</p>
+          </footer>
+    </body>
+</html>
